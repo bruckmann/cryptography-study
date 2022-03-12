1e0dc2d10f6ad2c45336246957c23230baecfbbc
Felipe Bruckmann <felipebruckmann@hotmail.com.br>
2022-03-12 19:15:44 -0300

    Refactoring to more didatical model of algoritm

--- src/caesarcipher.go
+++ src/caesarcipher.go
@@ -1,15 +1,36 @@
 package src
 
-func CeaserCipher(r rune, shift int) rune {
+//There are better ways to write this algorithm, but for educational purposes, we will do this way
 
-	// n[0] == a[0] = n[0] = a[3]
+func CeaserCipher(cleanText *string) string {
 
-	s := int(r) + shift
+	// n[0] == a[0] = n[0] = a[0 + 3]
 
-	if s > 'z' {
-		return rune(s - 26)
-	} else if s < 'a' {
-		return rune(s + 26)
+	chars := []rune(*cleanText)
+	shiftValue := 3
+
+	alphabet := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+
+	var cipherText string
+
+	for i := 0; i < len(chars); i++ {
+
+		if string(chars[i]) == " " {
+			cipherText += " "
+			continue
+		}
+
+		for j := 0; j < len(alphabet); j++ {
+
+			if string(chars[i]) == alphabet[j] {
+				if j+shiftValue > len(alphabet) {
+					replacementValue := j + shiftValue - len(alphabet)
+					cipherText += alphabet[replacementValue]
+					continue
+				}
+				cipherText += alphabet[j+shiftValue]
+			}
+		}
 	}
-	return rune(s)
+	return cipherText
 }
--- tests/caesarcipher_test.go
+++ tests/caesarcipher_test.go
@@ -1,20 +1,16 @@
 package tests
 
 import (
-	"strings"
 	"testing"
 
 	c "github.com/bruckmann/cryptography-study/src"
 )
 
 func TestCaesarCipher(t *testing.T) {
-	var cleanText string = "meet me after the toga party"
+	cleanText := "test the caesar cipher"
+	cipherText := "whvw wkh fdhvdu flskhu"
 
-	var cipherText string = "phhw=ph=diwhu=wkh=wrjd=sduwb"
-
-	got := strings.Map(func(r rune) rune {
-		return c.CeaserCipher(r, 3)
-	}, cleanText)
+	got := c.CeaserCipher(&cleanText)
 
 	if got != cipherText {
 		t.Errorf("Want %s but received %s", cipherText, got)
