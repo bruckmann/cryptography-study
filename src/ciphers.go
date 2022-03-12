ce0a366bda4441ed8498b0978770373bc1ca138b
Felipe Bruckmann <felipebruckmann@hotmail.com.br>
2022-03-12 15:18:25 -0300

    making caesar cipher using runes

--- src/ciphers.go
+++ src/ciphers.go
@@ -1,33 +1,15 @@
 package src
 
-func CeaserCipher(cleanText *string) string {
+func CeaserCipher(r rune, shift int) rune {
 
 	// n[0] == a[0] = n[0] = a[3]
-	chars := []rune(*cleanText)
 
-	alphabet := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
-	REPLACEMENT_NUMBER := 3
+	s := int(r) + shift
 
-	var cipherText string
-
-	for i := 0; i < len(chars); i++ {
-
-		if string(chars[i]) == " " {
-			cipherText += " "
-			continue
-		}
-
-		for j := 0; j < len(alphabet); j++ {
-
-			if string(chars[i]) == alphabet[j] {
-				if j+REPLACEMENT_NUMBER > len(alphabet) {
-					replacementValue := j + REPLACEMENT_NUMBER - len(alphabet)
-					cipherText += alphabet[replacementValue]
-					continue
-				}
-				cipherText += alphabet[j+REPLACEMENT_NUMBER]
-			}
-		}
+	if s > 'z' {
+		return rune(s - 26)
+	} else if s < 'a' {
+		return rune(s + 26)
 	}
-	return cipherText
+	return rune(s)
 }
--- tests/ciphers_test.go
+++ tests/ciphers_test.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"strings"
 	"testing"
 
 	c "github.com/bruckmann/cryptography-study/src"
@@ -9,8 +10,11 @@ import (
 func TestCaesarCipher(t *testing.T) {
 	var cleanText string = "meet me after the toga party"
 
-	var cipherText string = "phhw ph diwhu wkh wrjd sduwb"
-	got := c.CeaserCipher(&cleanText)
+	var cipherText string = "phhw=ph=diwhu=wkh=wrjd=sduwb"
+
+	got := strings.Map(func(r rune) rune {
+		return c.CeaserCipher(r, 3)
+	}, cleanText)
 
 	if got != cipherText {
 		t.Errorf("Want %s but received %s", cipherText, got)
